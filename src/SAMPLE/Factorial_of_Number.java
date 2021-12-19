86585141b2a3974c3076acf60da7ae872ea1253f
Admin <Admin@DESKTOP-GI61VKK>
2021-12-19 22:53:37 +0530

    KKKK

---
+++ src/SAMPLE/Factorial_of_Number.java
@@ -0,0 +1,11 @@
+package SAMPLE;
+
+public class Factorial_of_Number {
+	public static void main (String[]args){
+		int num=5;
+		int fact=1;
+		for(int i=num;i>0;i--){
+		fact=fact*i;
+		}System.out.println(fact);
+		}
+}
